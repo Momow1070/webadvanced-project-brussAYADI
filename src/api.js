0765351168e006faa02cedb47ba00c9b27e5ff5e
Momow1070 <150062232+Momow1070@users.noreply.github.com>
2026-05-06 14:34:17 +0200

    Maken van api.js voor records
    Ik heb hier mijn eerste js file gemaakt, om data te fetchen van Brussels API.
    Ik heb hier een constant gebruikt voor een limiet in aantal records, en een try block(met catch) gebruikt.

---
+++ src/api.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Hier zijn Constants, Arrow functies, Promises en async & await
+// Ik zal Brussels API gebruiken
+
+// Hier gebruik ik Constants voor de API en een integer als limiet voor aantal records.
+const API_URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/lieux_culturels_touristiques_evenementiels_visitbrussels_vbx/records";
+const LIMIT = 100; // Ik zal genoeg records fetchen zodat filtering goed werkt
+
+// Hier gebruik ik async en arrow functies
+export const fetchPlekken = async () => {
+    try{
+        const response = await fetch(`${API_URL}?limit=${LIMIT}`)
+
+        if(! response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.results || [];
+    }catch(error) {
+        console.error("Fout bij het ophalen van de data: ", error);
+        return []; // Leeg array als het niet lukt
+    }
+}
+
+
+
