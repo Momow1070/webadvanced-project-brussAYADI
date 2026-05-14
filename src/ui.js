5b430bc218624ce14947689bf461bdf6967f920e
Momow1070 <150062232+Momow1070@users.noreply.github.com>
2026-05-14 11:59:39 +0200

    Implementatie van Observer API
    Implementatie van Observer API voor "fade in" effect voor mijn kaarten bij downscrolling

--- src/ui.js
+++ src/ui.js
@@ -142,6 +142,28 @@ export const toggleLoading = (isLoading) => {
   }
 };
 
+// Observer API, zal mijn kaarten visuel "fade in" wanneer we downscrollen
+
+const setupIntersectionObserver = () => {
+  const cards = document.querySelectorAll('.venue-card');
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  cards.forEach(card => observer.observe(card));
+};
 
 
 
