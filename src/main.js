3b11439e359aa39937bf8fc257d46d3428aab7eb
Momow1070 <150062232+Momow1070@users.noreply.github.com>
2026-05-15 16:27:10 +0200

    Functies voor viewupdating en favoriet-systeem

--- src/main.js
+++ src/main.js
@@ -14,3 +14,25 @@ const categoryFilter = document.getElementById('categoryFilter');
 const gemeenteFilter = document.getElementById('gemeenteFilter');
 const sortSelect = document.getElementById('sortSelect');
 
+// UI updaten op basis van huidige staat 
+const updateView = () => {
+    const filteredData = applyFilters(allVenues, activeFilters);
+  renderVenues(filteredData, handleFavoriteClick);
+  renderFavoritesList(handleRemoveFavoriteClick);
+};
+
+// "Favoriet" in kaarten laten werken 
+
+const handleFavoriteClick = (venue) => {
+    const isNowfav = toggleFavorite(venue);
+    renderFavoritesList(handleRemoveFavoriteClick);
+    return isNowfav;
+};
+
+const handleRemoveFavoriteClick = (id) => {
+    toggleFavorite({ id });
+    updateView();
+};
+
+
+
